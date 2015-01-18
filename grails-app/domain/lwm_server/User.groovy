1066726f4825fca77500b5384933bb99f94f7b21
sonerik <thesonerik@gmail.com>
2015-01-18 21:07:47 +0200

    User's avatar url.

--- grails-app/domain/lwm_server/User.groovy
+++ grails-app/domain/lwm_server/User.groovy
@@ -5,11 +5,13 @@ class User {
     String name;
     String email;
     String password;
+    String avatar_url;
 
     static constraints = {
-        name size: 2..50, nullable: true
+        name size: 2..50, blank: true
         email email: true, unique: true, blank: false
         password size: 8..20, blank: false
+        avatar_url nullable: true
     }
 
 }
