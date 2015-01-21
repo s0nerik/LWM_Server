0ecf0f3263e53fde950bb3b3ef937434067a3263
sonerik <thesonerik@gmail.com>
2015-01-21 08:36:08 +0200

    Switched to PostgreSQL for development environment.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,13 @@
 dataSource {
     pooled = true
     jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
+//    driverClassName = "org.h2.Driver"
+    driverClassName = "org.postgresql.Driver"
+//    username = "sa"
+    username = "sonerik"
+
+    dialect = "org.hibernate.dialect.PostgreSQLDialect"
+
     password = ""
 }
 hibernate {
@@ -19,7 +24,8 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:postgresql://localhost:5432/lwm"
         }
     }
     test {
