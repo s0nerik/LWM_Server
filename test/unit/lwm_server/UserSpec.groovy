09cc93a825e2441ede1d414839c2d86de4fe549e
sonerik <thesonerik@gmail.com>
2015-01-22 23:47:19 +0200

    User unit tests updated.

--- test/unit/lwm_server/UserSpec.groovy
+++ test/unit/lwm_server/UserSpec.groovy
@@ -15,7 +15,7 @@ class UserSpec extends Specification {
     def cleanup() {
     }
 
-    def "create new User"() {
+    def "create a new User"() {
         given:
         def u = new User(email: "sonerik@mail.ua", password: "12345678")
 
@@ -27,6 +27,53 @@ class UserSpec extends Specification {
 
         cleanup:
         u.delete()
+    }
+
+    def "can't create a new User with broken email"() {
+        given:
+        def u = new User(email: "tatae_4dc", password: "12345678")
+
+        when:
+        u.save()
+
+        then:
+        User.findAll {it.email == "tatae_4dc"}.size() == 0
+
+        cleanup:
+        u.delete()
+    }
+
+    def "validatation of a User with broken email should fail"() {
+        given:
+        def u = new User(email: "tatae_4dc", password: "12345678")
+
+        expect:
+        u.validate() == false
+    }
+
+    def "validatation of a User with too short password should fail"() {
+        given:
+        def u = new User(email: "sonerik@mail.ua", password: "123456")
+
+        expect:
+        u.validate() == false
+    }
+
+    def "can't create two or more Users with same email"() {
+        given:
+        def u1 = new User(name: "user1", email: "sonerik@mail.ua", password: "12345678")
+        def u2 = new User(name: "user2", email: "sonerik@mail.ua", password: "123456789")
+
+        when:
+        u1.save(flush: true, validate: true)
+        u2.save(flush: true, validate: true)
+
+        then:
+        User.findAll { it.email == "sonerik@mail.ua" }.size() == 1
+
+        cleanup:
+        u1.delete()
+        u2.delete()
     }
 
 }
