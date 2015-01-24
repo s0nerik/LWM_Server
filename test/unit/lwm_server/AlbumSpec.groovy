a91c3e0d2c87e4604b1531bd84f5274cd668c9b8
sonerik <thesonerik@gmail.com>
2015-01-24 14:48:54 +0200

    Album unit tests.

--- test/unit/lwm_server/AlbumSpec.groovy
+++ test/unit/lwm_server/AlbumSpec.groovy
@@ -1,5 +1,6 @@
 package lwm_server
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Album)
+@Mock(Artist)
 class AlbumSpec extends Specification {
 
     def setup() {
@@ -15,6 +17,30 @@ class AlbumSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test validating an Album without an Artist"() {
+        given:
+        Album a = new Album(title: "Master Of Puppets")
+
+        expect:
+        a.validate() == false
     }
+
+    void "test validating an Album without a title"() {
+        given:
+        Artist artist = new Artist(name: "Asking Alexandria")
+        Album a = new Album(artist: artist)
+
+        expect:
+        a.validate() == false
+    }
+
+    void "test validating an Album with an Artist"() {
+        given:
+        Artist artist = new Artist(name: "Asking Alexandria")
+        Album a = new Album(title: "From Death To Destiny", artist: artist)
+
+        expect:
+        a.validate() == true
+    }
+
 }
