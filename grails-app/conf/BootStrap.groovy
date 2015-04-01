3e9741b6dafda48494075cf6c45ff4a309ce79bf
sonerik <thesonerik@gmail.com>
2015-04-01 17:49:36 +0300

    Cleaned BootStrap.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,14 +5,6 @@ class BootStrap {
     def init = { servletContext ->
         new Role(authority: 'ROLE_ADMIN').save()
         new Role(authority: 'ROLE_USER').save()
-
-//        JSON.registerObjectMarshaller(Settings) { // Remove 'class' entry from all JSON's
-//            return it.properties.findAll {k,v -> k != 'class'}
-//        }
-//
-//        JSON.registerObjectMarshaller(User) { // Remove 'class' entry from all JSON's
-//            return it.properties.findAll {k,v -> k != ('class' || 'accountExpired' || 'accountLocked')}
-//        }
     }
     def destroy = {
     }
